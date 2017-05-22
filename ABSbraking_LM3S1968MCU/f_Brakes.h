6d690d45cf6998c25bc12e320cfe5cfb5a165c4b
Nathan Pk <nathan.park@taitradio.com>
2017-05-23 09:24:04 +1200

    ABS braking Kernel (32bit ARM Cortex M3)

---
+++ ABSbraking_LM3S1968MCU/f_Brakes.h
@@ -0,0 +1,18 @@
+// Author: Nathanael Park and Hayden Becker
+// f_ Brakes Header for ENCE361 Project 2
+
+#ifndef F_BRAKES_H_
+#define F_BRAKES_H_
+
+// Includes
+#include "f_main.h"
+
+// Function prototypes
+void PortBIntHandler();
+void changeBrakeDuty();
+void checkBrakeNoSignal();
+uint16_t getBrakeStatus();
+uint16_t getBrakeInputHigh();
+
+
+#endif /* F_BRAKES_H_ */
---
+++ ABSbraking_LM3S1968MCU/f_scheduler.h
@@ -0,0 +1,16 @@
+// Author: Nathanael Park and Hayden Becker
+// Scheduler header for ENCE361 Project 2
+
+#ifndef SCHEDULER_H_
+#define SCHEDULER_H_
+
+// Includes
+#include "f_main.h"
+#include "f_display.h"
+#include "f_buttons.h"
+
+// Function prototypes
+uint16_t schedulerRun(void);
+void schedulerTask(float*, float, float, uint16_t, uint16_t, float*, uint16_t, uint16_t, float*);
+
+#endif /*SCHEDULER_H_*/
