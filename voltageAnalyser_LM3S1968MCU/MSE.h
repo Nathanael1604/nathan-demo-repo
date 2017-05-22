e5556b3cff863d257fd1e37bd098fa3bef0be2e7
Nathan Pk <nathan.park@taitradio.com>
2017-05-23 09:08:36 +1200

    Voltage Analyser Project (32bit ARM Cortex M3)

---
+++ voltageAnalyser_LM3S1968MCU/MSE.h
@@ -0,0 +1,13 @@
+// Author: Nathanael Park and Hayden Becker
+// MSE Module Header for ENCE361 Project 1
+
+#ifndef MSE_H_
+#define MSE_H_
+
+// Includes
+#include "my_adc.h"
+
+// Function prototypes
+uint16_t getMSE (uint16_t*, uint16_t*);
+
+#endif
---
+++ voltageAnalyser_LM3S1968MCU/frequency.h
@@ -0,0 +1,15 @@
+// Author: Nathanael Park and Hayden Becker
+// Frequency Header for ENCE361 Project 1
+
+#ifndef FREQUENCY_H_
+#define FREQUENCY_H_
+
+// Includes
+#include "my_adc.h"
+#include "peak_to_peak.h"
+#include "waveform.h"
+
+// Function prototypes
+uint16_t getFrequency(uint16_t*, uint16_t);
+
+#endif /*FREQUENCY_H_*/
